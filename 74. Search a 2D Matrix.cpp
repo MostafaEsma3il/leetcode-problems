babf9980356f285f575ac81e1c507df12e6dc904
Mostafa Esmail <100853809+MostafaEsma3il@users.noreply.github.com>
2023-04-30 17:22:15 +0200

    Create 74. Search a 2D Matrix.cpp

---
+++ 74. Search a 2D Matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        // Time Complexity: O(n + m);
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int md = (r + l) / 2;
+            if (matrix[md][0] <= target && matrix[md][m-1] >= target)
+                return row(m, md, matrix, target);
+            else if (matrix[md][m-1] < target)
+                l = md + 1;
+            else
+                r = md - 1;
+        }
+        return 0;
+    }
+
+    bool row (int m, int i, vector<vector<int>>& matrix, int target) {
+        int l = 0, r = m - 1;
+        while (l <= r) {
+            int md = (r + l) / 2;
+            if (matrix[i][md] == target)
+                return true;
+            else if (matrix[i][md] < target)
+                l = md + 1;
+            else 
+                r = md - 1;
+        }
+        return false;
+    }
+};
